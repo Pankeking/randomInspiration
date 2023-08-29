6661dd9d0a49931c3db029b2a9a69a05cca0f0e7
Javier <javiersoap@gmail.com>
2023-08-29 11:17:22 +0200

    border radius consistency

--- src/components/MainComp.js
+++ src/components/MainComp.js
@@ -9,13 +9,13 @@ import quotes from "../utils/quotes";
 
 function MainComp() {
 
-    const randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+    const randomColor = '#'+(0x1000000 +(Math.random())*0x777777).toString(16).substr(1,6);
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const [BgColor, setBgColor] = useState(randomColor);
     const [quote, setQuote] = useState(quotes[randomIndex]);
 
     function handleClick() {
-        const randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+        const randomColor = '#'+(0x1000000 +(Math.random())*0x777777).toString(16).substr(1,6);
         const randomIndex = Math.floor(Math.random() * quotes.length);
         setQuote(quotes[randomIndex]);
         setBgColor(randomColor);
